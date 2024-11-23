b45667d5995571ec1b70ffc8d4c8f96d8b8dd6c4
SALV <a01707122@tec.mx>
2024-11-23 17:14:46 -0600

    FIX routes

--- backend/src/routes/view.ts
+++ backend/src/routes/view.ts
@@ -1,9 +1,8 @@
 import { Router } from "express";
-import { getCam, getPhoto, borrego, welcome } from "../services/view";
+import { getCam, borrego, welcome } from "../services/view";
 const router = Router();
 
 router.get("/", getCam);
-router.get("/get_photo", getPhoto);
 router.get("/get_photo", borrego);
 router.get("/get_photo", welcome);
 
--- frontend/scripts/home.ts
+++ frontend/scripts/home.ts
@@ -3,11 +3,18 @@ function analyzeImg() {
   fetch("http://localhost:3000/whereAmI")
     .then((response) => response.text())
     .then((data) => {
-      document.getElementById("result").textContent = data;
-      console.log("Respuesta:", data);
+      const result = document.getElementById("result");
+      if (result) {
+        result.textContent = data;
+        console.log("Respuesta:", data);
+      }
     })
     .catch((error) => {
-      document.getElementById("result").textContent = "Error: " + error;
+      const result = document.getElementById("result");
+      if (result) {
+        result.textContent = error;
+        console.log("Error:", error);
+      }
       console.error("Error:", error);
     });
 }
