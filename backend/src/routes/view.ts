55b30edae9758a1a22363c9d96564c0f48cc7858
SALV <a01707122@tec.mx>
2024-11-23 17:34:16 -0600

    redirección guiame

--- backend/src/routes/view.ts
+++ backend/src/routes/view.ts
@@ -3,7 +3,7 @@ import { getCam, borrego, welcome } from "../services/view";
 const router = Router();
 
 router.get("/", getCam);
-router.get("/get_photo", borrego);
-router.get("/get_photo", welcome);
+router.get("/borrego", borrego);
+router.get("/welcome", welcome);
 
 export default router;
