afd8879b588aa6c5e46d1cb3d4005bf991cd51ba
kamimartinez <kamiii.martineeez@gmail.com>
2024-11-23 16:58:29 -0600

    [FIX] Image Recognition Botton Index

--- frontend/scripts/home.ts
+++ frontend/scripts/home.ts
@@ -1,5 +1,13 @@
-const analyzeImg = async () => {
+// frontend/scripts/home.js
+function analyzeImg() {
   fetch("http://localhost:3000/whereAmI")
-    .then((response) => console.log("response: " + response))
-    .catch((error) => console.error("Error: " + error));
-};
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("result").textContent = data;
+      console.log("Respuesta:", data);
+    })
+    .catch((error) => {
+      document.getElementById("result").textContent = "Error: " + error;
+      console.error("Error:", error);
+    });
+}
